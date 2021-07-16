960477bb2f56b9ba1f8e48bb7c6b2dd54035b677
YunYouJun <YunYouJun@users.noreply.github.com>
2021-07-16 14:05:32 +0000

    deploy: ce0351ba4ef9153c5db0888e7b715fd4d5363ebe

---
+++ precache-manifest.f41ffa898239dbe4e5a08c820c4f2983.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ed3ab90ca7d22e8221ee8a0ea4d517",
+    "url": "/birthday/audio/happy-birthday.mp3"
+  },
+  {
+    "revision": "6e164a7a1d7b7ff14353",
+    "url": "/birthday/css/about.17cf9d48.css"
+  },
+  {
+    "revision": "e9a77f963e9e066b9396",
+    "url": "/birthday/css/index.2b223d6c.css"
+  },
+  {
+    "revision": "6800561306331fa0e812286bbe6928db",
+    "url": "/birthday/img/birthday.png"
+  },
+  {
+    "revision": "5335536a83d6822aa47226aa9e83efbb",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "6e164a7a1d7b7ff14353",
+    "url": "/birthday/js/about.d470995b.js"
+  },
+  {
+    "revision": "6b9d3d4fa4151b4b0c63",
+    "url": "/birthday/js/chunk-vendors.99432508.js"
+  },
+  {
+    "revision": "e9a77f963e9e066b9396",
+    "url": "/birthday/js/index.e5c9c014.js"
+  },
+  {
+    "revision": "72e8a95752985a2f05b8ce283f3b7738",
+    "url": "/birthday/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/birthday/robots.txt"
+  }
+]);
